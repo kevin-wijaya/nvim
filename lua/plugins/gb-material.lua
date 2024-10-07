ea8e49fdfc36595b5bc3c4fcc6e3b77fbcf3b617
kevin-wijaya <yohaneskevinwijaya@gmail.com>
2024-10-07 19:50:48 +0700

    Updated Plugins

--- lua/plugins/gb-material.lua
+++ lua/plugins/gb-material.lua
@@ -1,10 +1,12 @@
 return {
 	{
 		"sainnhe/gruvbox-material",
+		event = "VimEnter",
 		lazy = false,
 		priority = 1000,
 		opts = function () 
-		    vim.g.gruvbox_material_enable_italic = true,
+			vim.g.gruvbox_material_transparent_background = 0
+			vim.g.gruvbox_material_enable_italic = true
 			vim.cmd.colorscheme('gruvbox-material')
 		end
 	}
